05bf6fa994fcaa6509a6f0c23ea9ac720dcd6b49
Jose-Roman <josejroman@cpp.edu>
2021-08-19 15:33:44 -0700

    Initial commit

---
+++ src/com/company/georgie.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class georgie
+{
+    public static void main(String[] args)
+    {
+      int sum = 0;
+      for(int i = 0; i <= 50;i+=2)
+      {
+          sum = sum + (i*i);
+      }
+    System.out.println(sum);
+    }
+
+
+
+}
